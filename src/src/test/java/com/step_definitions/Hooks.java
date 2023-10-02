e196b1f3ffd4d0137d180126d50e7a8e323313d8
ilhanTabak <herr.ilhantabak@gmail.com>
2023-10-02 23:06:04 +0200

    firstIssue

---
+++ src/src/test/java/com/step_definitions/Hooks.java
@@ -0,0 +1,30 @@
+package com.step_definitions;
+
+import com.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+import java.util.concurrent.TimeUnit;
+
+public class Hooks {
+    @Before  //JAVA
+    public void setUp(){
+
+        Driver.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        Driver.get().manage().window().maximize();
+    }
+    @After
+    public void tearDown(){
+        //System.out.println("\tThis is coming from after method");
+        //Driver.closeDriver();
+    }
+
+//    @Before("@db")
+//    public void setUpDB(){
+//        System.out.println("t connection DB");
+//    }
+//    @After("@db")
+//    public void tearDownDB(){
+//        System.out.println("\tDisconnection DB");
+//    }
+}
---
+++ src/src/test/java/com/step_definitions/Login_step_def.java
@@ -0,0 +1,36 @@
+package com.step_definitions;
+
+import com.pages.LoginPage;
+import com.utilities.BrowserUtils;
+import com.utilities.ConfigurationReader;
+import com.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login_step_def {
+        LoginPage loginPage = new LoginPage();
+        WebDriver driver;
+
+
+        @Given("The user on the login page")
+        public void the_user_on_the_login_page() {
+                Driver.get().get(ConfigurationReader.get("url"));
+        }
+
+        @When("The user enter valid credentials")
+        public void theUserEnterValidCredentials() {
+                loginPage.loginMethod();
+
+        }
+
+        @Then("The user can log in")
+        public void the_user_can_log_in() {
+
+        }
+
+
+}
