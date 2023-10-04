20554b80320aa61cc9eb464a167ad95972037b86
Hamza <hamzasahinofficial@gmail.com>
2023-10-04 21:09:28 +0200

    Hamza Sahin - Mein erste Commit

--- src/test/java/com/pages/LoginPage.java
+++ src/test/java/com/pages/LoginPage.java
@@ -10,60 +10,40 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends BasePage {
-
-
     WebDriver driver;
-
 
     @FindBy(css = "[id='cookie-ok']")
     public WebElement acceptButton;
-
-@FindBy(css = "[class='icon-user']")
+    @FindBy(css = "[class='icon-user']")
     public WebElement userBox;
-
     @FindBy(css = "[class='userlink-login']")
-    public WebElement userLogin;
-
+    public WebElement anmelden;
     @FindBy(css = "[type='email']")
     public WebElement userName;
     @FindBy(css = "[type='password']")
     public WebElement password;
-
     @FindBy(css = "[id='LoginBtn']")
-    public WebElement submit;
-
+    public WebElement anmeldenButton;
     @FindBy(css = "[title='Mein Konto']")
     public WebElement meinKonto;
 
+    public void loginMethod() {
+        BrowserUtils.waitFor(2);
+        acceptButton.click();
+        BrowserUtils.waitFor(2);
+        userBox.click();
+        BrowserUtils.waitFor(2);
+        anmelden.click();
 
-    public void loginMethod(){
-        BrowserUtils.waitFor(2);
-      acceptButton.click();
-        BrowserUtils.waitFor(2);
-       userBox.click();
-        BrowserUtils.waitFor(2);
-      userLogin.click();
-
-        driver= Driver.get();
+        driver = Driver.get();
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(0,200);");
 
-        userName.sendKeys(ConfigurationReader.get("username"));
-        password.sendKeys(ConfigurationReader.get("password"));
+        userName.sendKeys(ConfigurationReader.get("loginEmail"));
+        password.sendKeys(ConfigurationReader.get("loginPassword"));
         BrowserUtils.waitFor(2);
-        submit.click();
-       //------------------------------------------------------------
+        anmeldenButton.click();
 
-        Assert.assertEquals("Mein Konto",meinKonto.getText());
-
-
-
-
+        Assert.assertEquals("Mein Konto", meinKonto.getText());
     }
-
-
-
-
-
-
 }
